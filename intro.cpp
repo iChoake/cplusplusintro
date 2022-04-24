5be2e1efa314560dfa4101b41dd532878592c36e
Steven <45870644+iChoake@users.noreply.github.com>
2022-04-24 09:48:56 -0400

    Create intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+string name="",city="",state="",zip="",phone="",major=""; //Fill with Info
+cout<<"Name : "<<name<<"\nAddresss : "<<city<<", "<<state<<", "<<zip<<"\nTelephone : "<<phone<<"\nCollege Major :"<<major;
+return 0;
+}
